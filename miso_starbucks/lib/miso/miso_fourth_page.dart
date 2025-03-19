2bf430838d3e0e89d6f7a28f2870949db3e7cb60
complete0415jiyoung <complete0415@naver.com>
2025-03-19 19:48:53 +0900

    FEAT: miso세번째 네번째 페이지

--- miso_starbucks/lib/miso/miso_fourth_page.dart
+++ miso_starbucks/lib/miso/miso_fourth_page.dart
@@ -6,6 +6,63 @@ class MisoFourthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Miso 네 번째 페이지")));
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 62),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "나의 정보",
+                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 32),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "010-1234-5678",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 32),
+              ListTile(
+                onTap: () => print("주소 관리 클릭 됨"),
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.home_outlined, color: Colors.black),
+                title: Text(
+                  "주소 관리",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              ListTile(
+                onTap: () => print("결제 수단 관리 클릭 됨"),
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.credit_card_outlined, color: Colors.black),
+                title: Text(
+                  "결제 수단 관리",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              ListTile(
+                onTap: () => print("공지사항 클릭 됨"),
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.volume_mute_outlined, color: Colors.black),
+                title: Text(
+                  "공지사항",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+              ListTile(
+                onTap: () => print("문의사항 클릭 됨"),
+                contentPadding: EdgeInsets.all(0),
+                leading: Icon(Icons.help_outline, color: Colors.black),
+                title: Text(
+                  "문의사항",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- miso_starbucks/lib/miso/miso_third_page.dart
+++ miso_starbucks/lib/miso/miso_third_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:miso_starbucks/app_color.dart';
 
+/// 세 번째 페이지
 /// 세 번째 페이지
 class MisoThirdPage extends StatelessWidget {
   const MisoThirdPage({Key? key}) : super(key: key);
@@ -10,6 +12,115 @@ class MisoThirdPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Miso 세 번째 페이지")));
+    return Scaffold(
+      backgroundColor: AppColor.misoPrimaryColor,
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              /// 배경 이미지
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  // 이미지 최대 크기 제한
+                  constraints: BoxConstraints(maxWidth: 400),
+                  child: Image.network(backgroundImgUrl),
+                ),
+              ),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: 64),
+
+                  /// 타이틀
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      // 공통 스타일
+                      style: TextStyle(
+                        fontSize: 28,
+                        height: 1.5, // 줄간격 한 줄의 1.5배
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(text: "친구 추천할 때마다\n"),
+                        TextSpan(
+                          text: "10,000원 ",
+                          // 개별 스타일
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(text: "할인 쿠폰 지급!"),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 64),
+
+                  /// 자세히 보기
+                  GestureDetector(
+                    onTap: () {
+                      print("자세히 보기 클릭 됨");
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "자세히 보기",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Icon(Icons.chevron_right, color: Colors.white),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              /// 친구 추천하기
+              Positioned(
+                bottom: 42,
+                child: GestureDetector(
+                  onTap: () {
+                    print("친구 추천하기 클릭 됨");
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(64),
+                      // 그림자
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.4),
+                          // 광원의 위치를 중심(0, 0)보다 위쪽(0, 5133)
+                          offset: Offset(0, 5),
+                          spreadRadius: 1,
+                          blurRadius: 12,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.redeem, color: AppColor.misoPrimaryColor),
+                        SizedBox(width: 8),
+                        Text(
+                          "친구 추천하기",
+                          style: TextStyle(
+                            color: AppColor.misoPrimaryColor,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
